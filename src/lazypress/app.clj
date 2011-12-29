337e6cff584ceb510b4ddbde1b3e01f6a29173f0
Sun Ning <classicning@gmail.com>
2011-12-29 21:28:01 +0800

    restruct code

--- src/lazypress/app.clj
+++ src/lazypress/app.clj
@@ -1,56 +1,13 @@
 (ns lazypress.app
-  (:use [lazypress vmc utils])
+  (:use [lazypress vmc utils views])
   (:use [compojure core route handler])
-  (:use [net.cgrand.enlive-html])
   (:use [somnium.congomongo])
-  (:use [ring.util.response])
   (:use [clojure.string :only [blank? lower-case]])
   (:import [java.util Date])
   (:import [org.apache.commons.codec.digest DigestUtils]))
 
 (declare db-conn)
 
-(deftemplate index "index.html"
-  []
-  )
-(deftemplate page "page.html"
-  [ctx]
-  [:div#page-body] (html-content (:content ctx))
-  [:a#author-display] (do->
-                          (content (if-not (blank? (:author ctx))
-                                     (:author ctx) "anonymous"))
-                          (set-attr :href
-                                    (if-not (blank? (:author ctx))
-                                      (str "/a/" (:author ctx))
-                                      "#")))
-  [:span#pubdate] (content (.toString (:date ctx)))
-  [:span#title] (content (if-not (blank? (:title ctx))
-                           (:title ctx) "untitled"))
-  [:title] (content (:title ctx) " - LazyPress")
-  [:input#id] (set-attr :value (:id ctx))
-  [:input#author] (set-attr :value (:author ctx))
-  [:div#author-box] (if (blank? (:author ctx)) (html-content "") identity))
-
-(deftemplate edit "edit.html"
-  [ctx]
-  [:textarea#content] (content (:content ctx))
-  [:input#id] (set-attr :value (:id ctx))
-  [:input#author] (set-attr :value (:author ctx))
-  [:input#title] (set-attr :value (:title ctx))
-  [:title] (content (:title ctx) " - LazyPress"))
-
-
-(defsnippet article-model "author.html"
-  [:.article-item]
-  [ctx]
-  [:a.article-link] (do-> (content (:title ctx))
-                          (set-attr :href (str "/p/" (:id ctx))))
-  [:span.article-date] (content (.toString (:date ctx))))
-
-(deftemplate author "author.html"
-  [ctx]
-  [:h2#author-name] (content (:author ctx))
-  [:ul#article-list] (content (map article-model (:pages ctx))))
 
 (defn view-index [req]
   (index))
---
+++ src/lazypress/views.clj
@@ -0,0 +1,47 @@
+(ns lazypress.views
+  (:use [net.cgrand.enlive-html])
+  (:use [clojure.string :only [blank?]]))
+
+(deftemplate index "index.html"
+  []
+  )
+(deftemplate page "page.html"
+  [ctx]
+  [:div#page-body] (html-content (:content ctx))
+  [:a#author-display] (do->
+                          (content (if-not (blank? (:author ctx))
+                                     (:author ctx) "anonymous"))
+                          (set-attr :href
+                                    (if-not (blank? (:author ctx))
+                                      (str "/a/" (:author ctx))
+                                      "#")))
+  [:span#pubdate] (content (.toString (:date ctx)))
+  [:span#title] (content (if-not (blank? (:title ctx))
+                           (:title ctx) "untitled"))
+  [:title] (content (:title ctx) " - LazyPress")
+  [:input#id] (set-attr :value (:id ctx))
+  [:input#author] (set-attr :value (:author ctx))
+  [:div#author-box] (if (blank? (:author ctx)) (html-content "") identity))
+
+(deftemplate edit "edit.html"
+  [ctx]
+  [:textarea#content] (content (:content ctx))
+  [:input#id] (set-attr :value (:id ctx))
+  [:input#author] (set-attr :value (:author ctx))
+  [:input#title] (set-attr :value (:title ctx))
+  [:title] (content (:title ctx) " - LazyPress"))
+
+
+(defsnippet article-model "author.html"
+  [:.article-item]
+  [ctx]
+  [:a.article-link] (do-> (content (:title ctx))
+                          (set-attr :href (str "/p/" (:id ctx))))
+  [:span.article-date] (content (.toString (:date ctx))))
+
+(deftemplate author "author.html"
+  [ctx]
+  [:h2#author-name] (content (:author ctx))
+  [:ul#article-list] (content (map article-model (:pages ctx))))
+
+
